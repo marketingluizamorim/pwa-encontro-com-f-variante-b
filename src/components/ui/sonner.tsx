b31ada892c711a0fecffa130c95b70f0500d3230
marketingluizamorim <marketing.luizamorim@gmail.com>
2026-02-17 09:51:15 -0300

    style: adjust notification offset to 50px

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,7 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      offset="250px"
+      offset="50px"
       toastOptions={{
         classNames: {
           toast:
