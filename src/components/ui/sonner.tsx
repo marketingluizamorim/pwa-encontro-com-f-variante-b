53de33a27f9a41260b191d7fdb4d76e753e48b86
marketingluizamorim <marketing.luizamorim@gmail.com>
2026-02-17 09:19:49 -0300

    test: extreme 500px offset for notifications debugging

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,7 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      offset="120px"
+      offset="500px"
       toastOptions={{
         classNames: {
           toast:
